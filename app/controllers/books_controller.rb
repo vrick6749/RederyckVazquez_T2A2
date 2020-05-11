6715904669be2178ed12e506d85455148711e35c
rick <vrick6749@gmail.com>
2020-05-11 18:54:02 +1000

    made the controller with some method headers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+
+
+    def index
+        
+    end
+
+
+
+    def show
+
+    end
+
+
+
+
+
+
+
+
+
+
+
+    def new
+
+    end
+
+
+
+    def edit
+
+    end
+
+
+
+
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+
+
+
+
+
+
+
+
+    def delete
+
+    end
+
+
+end
