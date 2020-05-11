8eac56a3081f3a018bb4e9a1ff5b25e15d52d704
rick <vrick6749@gmail.com>
2020-05-11 19:48:31 +1000

    created seed author, publisher, genre tested database is now populated everything is working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+for i in 1..10
+
+    Author.create(
+    name:Faker::Book.author,
+    biography:Faker::Lorem.sentence(word_count: 25)
+    )
+
+
+end
+
+for i in 1..20
+    Publisher.create(
+        name:Faker::Book.publisher,
+        details:Faker::Lorem.sentence(word_count: 15)
+    )
+end
+
+for i in 1..15
+
+    Genre.create(
+        name:Faker::Book.genre
+    )
+
+
+end
+
