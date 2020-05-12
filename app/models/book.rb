e71e1efceed89225217d44b4fa474c9657d92ffe
rick <vrick6749@gmail.com>
2020-05-12 20:23:50 +1000

    added model specs for book and genre

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :author
   belongs_to :publisher
+  has_many :bookgenres
+  has_many :genres, through: :bookgenres
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,4 @@
 class Genre < ApplicationRecord
+    has_many :bookgenres
+    has_many :books, through: :bookgenres
 end
